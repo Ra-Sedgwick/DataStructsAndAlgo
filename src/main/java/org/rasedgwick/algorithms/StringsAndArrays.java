b15d3965f0076e93a5a91d3924d6f09a9f55e581
Robert Adam Sedgwick <rasedgwick@gmail.com>
2023-09-27 15:22:24 -0400

    Initial

---
+++ src/main/java/org/rasedgwick/algorithms/StringsAndArrays.java
@@ -0,0 +1,9 @@
+package org.rasedgwick.algorithms;
+
+public class StringsAndArrays {
+    public static void tryIt() {
+        System.out.println("Im Alive");
+    }
+
+
+}
---
+++ src/test/java/StringsAndArraysTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import org.rasedgwick.algorithms.StringsAndArrays;
+
+public class StringsAndArraysTests {
+    @Test
+    public void test(){
+        StringsAndArrays.tryIt();
+    }
+}
