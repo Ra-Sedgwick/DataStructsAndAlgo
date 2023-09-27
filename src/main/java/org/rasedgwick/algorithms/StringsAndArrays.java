9acb64dbfeb08b01a18156e10494b288af1124fa
Robert Adam Sedgwick <rasedgwick@gmail.com>
2023-09-27 16:42:25 -0400

    Wip Add failing String Tests

--- src/main/java/org/rasedgwick/algorithms/StringsAndArrays.java
+++ src/main/java/org/rasedgwick/algorithms/StringsAndArrays.java
@@ -1,12 +1,25 @@
 package org.rasedgwick.algorithms;
 
+import jdk.jshell.spi.ExecutionControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StringsAndArrays {
     static Logger logger = LoggerFactory.getLogger(StringsAndArrays.class);
-    public static void tryIt() {
-        logger.info("Im Alive");
+
+    // Determines if all characters in a string are unique.
+    public static boolean isUniqueChars(String str) {
+        return false;
+    }
+
+    // Given two strings determine if one is a permutation of the other
+    public static boolean isPermutation(String s, String t) {
+        return false;
+    }
+
+    // Give a string replace all white space with '%20`
+    public static String URLify(String s, int trueLength){
+        return  "";
     }
 
 
--- src/test/java/StringsAndArraysTests.java
+++ src/test/java/StringsAndArraysTests.java
@@ -1,9 +1,62 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.rasedgwick.algorithms.StringsAndArrays;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StringsAndArraysTests {
-    @Test
-    public void test(){
-        StringsAndArrays.tryIt();
+    @ParameterizedTest(name = "#{index} - Run isUniqueChars test with args = {0}")
+    @NullAndEmptySource
+    @ValueSource(strings = {"AaBbCc", "\\nbc"})
+    public void isUniqueCharsTest_False(String str){
+       boolean result = StringsAndArrays.isUniqueChars(str);
+       assertFalse(result);
+    }
+
+    @ParameterizedTest(name = "#{index} - Run isUniqueChars test with args = {0}")
+    @ValueSource(strings = {"a", "abc", " abc"})
+    public void isUniqueCharsTest_True(String str){
+        boolean result = StringsAndArrays.isUniqueChars(str);
+        assertTrue(result);
+    }
+
+
+    @ParameterizedTest(name = "#{index} - Run isPermutation test with args = {0}")
+    @CsvSource({
+            "abc, abcc",
+            "abc, ab c",
+            "abc, ab c ",
+            "ab2, abcd",
+            "\\nbc, \nbc"
+    })
+    public void isPermutationTest_Fasle(String s, String t){
+        boolean result = StringsAndArrays.isPermutation(s, t);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest(name = "#{index} - Run isPermutation test with args = {0}, {1}")
+    @CsvSource({
+            "abc, abc",
+            "a bc, ab c",
+            "ab2, 2bc",
+            "\\nbc, \\ncb"
+    })
+    public void isPermutationTest_True(String s, String t){
+        boolean result = StringsAndArrays.isPermutation(s, t);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest(name = "#{index} - Run URLify test with args = {0}")
+    @CsvSource({
+            "mytest.com/ab cd efg, mytest.com/ab%20cd%20efg",
+            " mytest.com/abcdefg , %20mytest.com/abcdefg%20",
+            "\"\", \"\"",
+    })
+    public void URLifyTest(String input, String expected){
+        String result = StringsAndArrays.URLify(input, input.length());
+        assertEquals(expected, result);
     }
 }
