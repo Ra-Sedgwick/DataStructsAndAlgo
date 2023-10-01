ef6f6bad4d37acfeb6dd48f95a3d8478bc03b8a1
Robert Adam Sedgwick <rasedgwick@gmail.com>
2023-10-01 09:25:34 -0400

    Add StringsAndArrays questions

--- src/main/java/org/rasedgwick/algorithms/StringsAndArrays.java
+++ src/main/java/org/rasedgwick/algorithms/StringsAndArrays.java
@@ -1,26 +1,164 @@
 package org.rasedgwick.algorithms;
 
-import jdk.jshell.spi.ExecutionControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StringsAndArrays {
     static Logger logger = LoggerFactory.getLogger(StringsAndArrays.class);
 
     // Determines if all characters in a string are unique.
-    public static boolean isUniqueChars(String str) {
-        return false;
+    public static Boolean isUniqueChars(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        //  If Extended ASCII
+        if (str.length() > 256) {
+            return false;
+        }
+
+        HashSet<Character> charSet = new HashSet<>();
+
+        char[] charArr = str.toCharArray();
+
+        for (char c : charArr) {
+            if (charSet.contains(c)) {
+                return false;
+            }
+            charSet.add(c);
+        }
+
+        return true;
     }
 
     // Given two strings determine if one is a permutation of the other
     public static boolean isPermutation(String s, String t) {
-        return false;
+        if (s == null ||
+            t == null ||
+            s.isEmpty() ||
+            t.isEmpty() ||
+            s.length() != t.length()
+        ) {
+            return false;
+        }
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        CharacterIterator sCI = new StringCharacterIterator(s);
+        CharacterIterator tCI = new StringCharacterIterator(t);
+
+        while (sCI.current() != CharacterIterator.DONE) {
+            Integer count = charCount.get(sCI.current());
+            if (count == null) {
+                count = 0;
+            } 
+
+            charCount.put(sCI.current(), ++count);
+
+            sCI.next();
+        }
+
+        // If the lengths are equal, and not all chars are the same, at least one element
+        // will be less than zero and one will be greater than.
+        while (tCI.current() != CharacterIterator.DONE) {
+            Integer count = charCount.get(tCI.current());
+            if (count == null) {
+                count = 0;
+            }
+
+            charCount.put(tCI.current(), --count);
+
+            if (charCount.get(tCI.current()) < 0) {
+                return false;
+            }
+            tCI.next();
+        }
+
+        return true;
     }
 
     // Give a string replace all white space with '%20`
-    public static String URLify(String s, int trueLength){
-        return  "";
+    public static String URLify(String s, int trueLength) {
+        // String.replace would be a trivial solution
+        final String encodedWhiteSpace = "%20";
+        final int offset = encodedWhiteSpace.length() - 1;
+        int length = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < length; i++) {
+            char c = sb.charAt(i);
+            if (Character.isWhitespace(c)) {
+                sb.replace(i, i+1, encodedWhiteSpace);
+                i += offset;
+                length += offset;
+            }
+        }
+        
+        
+        return sb.toString();
     }
+
+    // Palindrome Permutation (P.P.)
+    // Given a string check if its a permutation of a palindrome.
+    public boolean palindromePermutation(String str) {
+        if (str == null || str.isBlank()) {
+            return false;
+        }
+
+        // If the length is even, individual char counts must be even 'aabbcc' -> 'abccba'
+        // If the length is odd, individual char counts must be even, excluding a character that can have a count of 1, placed in the middle.  'aab' -> 'aba'
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        CharacterIterator ci = new StringCharacterIterator(str);
+        while (ci.current() != CharacterIterator.DONE) {
+            if (charMap.containsKey(ci.current())) {
+                charMap.put(ci.current(),  charMap.get(ci.current())  + 1);
+            } else {
+                charMap.put(ci.current(), 0) ;
+            }
+            ci.next();
+        }
+
+        // Logic can be simplified by making sure no more then one character has an odd count.
+        int oddCount = 0;
+        Collection<Integer> charCounts = charMap.values(); // I should have just used a array indexed with charters numeric value instead.
+        for (Integer i : charCounts) {
+            if (!isEven(i)) {
+                oddCount++;
+                if (oddCount > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- src/test/java/StringsAndArraysTests.java
+++ src/test/java/StringsAndArraysTests.java
@@ -10,29 +10,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringsAndArraysTests {
     @ParameterizedTest(name = "#{index} - Run isUniqueChars test with args = {0}")
     @NullAndEmptySource
-    @ValueSource(strings = {"AaBbCc", "\\nbc"})
-    public void isUniqueCharsTest_False(String str){
-       boolean result = StringsAndArrays.isUniqueChars(str);
-       assertFalse(result);
+    @ValueSource(strings = {"aa", "abcdd", "abcd a"})
+    public void isUniqueCharsTest_False(String str) {
+        boolean result = StringsAndArrays.isUniqueChars(str);
+        assertFalse(result);
     }
 
     @ParameterizedTest(name = "#{index} - Run isUniqueChars test with args = {0}")
-    @ValueSource(strings = {"a", "abc", " abc"})
-    public void isUniqueCharsTest_True(String str){
+    @ValueSource(strings = {"a", "abc", " abc", "AaBbCc", "\\nbc"})
+    public void isUniqueCharsTest_True(String str) {
         boolean result = StringsAndArrays.isUniqueChars(str);
         assertTrue(result);
     }
 
 
-    @ParameterizedTest(name = "#{index} - Run isPermutation test with args = {0}")
+    @ParameterizedTest(name = "#{index} - Run isPermutation test with args = {0}, {1}")
     @CsvSource({
-            "abc, abcc",
+            "aabc, abcd",
+            "abcd, abcc",
             "abc, ab c",
             "abc, ab c ",
             "ab2, abcd",
             "\\nbc, \nbc"
     })
-    public void isPermutationTest_Fasle(String s, String t){
+    public void isPermutationTest_Fasle(String s, String t) {
         boolean result = StringsAndArrays.isPermutation(s, t);
         assertFalse(result);
     }
@@ -41,10 +42,10 @@ public class StringsAndArraysTests {
     @CsvSource({
             "abc, abc",
             "a bc, ab c",
-            "ab2, 2bc",
+            "ab2, 2ba",
             "\\nbc, \\ncb"
     })
-    public void isPermutationTest_True(String s, String t){
+    public void isPermutationTest_True(String s, String t) {
         boolean result = StringsAndArrays.isPermutation(s, t);
         assertTrue(result);
     }
@@ -52,11 +53,18 @@ public class StringsAndArraysTests {
     @ParameterizedTest(name = "#{index} - Run URLify test with args = {0}")
     @CsvSource({
             "mytest.com/ab cd efg, mytest.com/ab%20cd%20efg",
-            " mytest.com/abcdefg , %20mytest.com/abcdefg%20",
             "\"\", \"\"",
     })
-    public void URLifyTest(String input, String expected){
+    public void URLifyTest(String input, String expected) {
         String result = StringsAndArrays.URLify(input, input.length());
         assertEquals(expected, result);
     }
+
+    @Test
+    public void URLifyTest() {
+        String url = " 123 ";
+        String result = StringsAndArrays.URLify(url, url.length());
+        assertEquals( "%20123%20", result);
+    }
+
 }
